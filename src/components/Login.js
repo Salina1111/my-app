f3bca2a117a87d4cee6ecab694bba41fc8e80e5c
Salina <salina.sallu@outlook.com>
2018-05-03 19:12:14 +0545

    Login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,42 +1,109 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 
 import TextField from 'material-ui/TextField';
-import { blue500 } from 'material-ui/styles/colors';
 import RaisedButton from 'material-ui/RaisedButton';
+import {Card} from 'material-ui/Card';
+import Done from 'material-ui/svg-icons/communication/vpn-key';
 
-const style={
-	 margin: 12
-}
-
-const floatingLabelStyle={
-    color: blue500
-}
-
-const floatingLabelFocusStyle={
-    color: blue500
-}
-
+const style = {
+    margin: 12,
+  };
 
 
 class Login extends Component{
-	render(){
-		return(
-			<div>
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            apikey: ''
+        }
+    }
+    alignCenter = {
+        height: "100vh",
+        display: "flex",   
+        justifyContent: "center", 
+        alignItems: "center"
+    }
 
-			    <TextField
-			      
-			      hintText="Email"
-			    /><br />
+    cardWidth = {
+        width: "500px"
+    }
 
-			    <TextField
-			      hintText="API key"
-			      type="password"
-			    /><br />
-			    
-			  </div>
-	  	);
-	}
+    render() {
+        return(
+             <div style={this.alignCenter}>
+                
+               <Card style= {this.cardWidth} > 
+                    <TextField
+                    hintText="Enter you email"
+                    floatingLabelText="Email"
+                    type="email"
+                    value={this.state.email}
+                    onChange={(e) => this.onEmailChange(e)}
+                    />
+
+                    <br />
+                    <br />
+                   
+
+                    <TextField
+                    hintText="Enter your API Key"
+                    type="password"
+                    floatingLabelText="API Key"
+                    value={this.state.apikey}
+                    onChange={(e) => this.onApikeyChange(e)}
+                    />
+
+                    <br />
+
+                    <RaisedButton label="Login" primary={true} style={style}
+                    icon= {<Done />}
+                    labelPosition="before"
+                    onClick={() => this.onSubmit()}
+                 />
+
+                 </Card>   
+        </div>
+        )
+    };
+    
+    onEmailChange(e) {
+        this.setState({email: e.target.value})
+    }
+
+onApikeyChange(e){
+    this.setState({apikey: e.target.value})
+}
+
+onSubmit(){
+    fetch('https://api.rebrandly.com/v1/account',
+{
+    headers: {
+        apikey: this.state.apikey
+    }
+})
+.then(response => {
+    if(response.ok) {
+        response.json()
+        .then(data => {
+            console.log(data)
+            if(data.email ===this.state.email){
+                console.log("Right User")
+            }
+            else{
+                alert("Not authorized user")
+            }
+        })
+    }
+  
+    else{
+        alert(response.statusText)
+    }
+    })
+   
+}
+
 }
 
 
-export default Login;	
+export default Login;
